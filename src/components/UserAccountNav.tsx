49008135fda81c52d3582fede8df2d4e37dcd778
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-08-26 22:19:26 +0530

    lucid react package installed

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,13 +1,56 @@
+'use client'
 import React from 'react'
-import { User } from '@prisma/client'
+import { User } from 'next-auth'
+import { signOut } from "next-auth/react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu'
+import Link from 'next/link'
+import { Button } from './ui/button'
+
 
 type Props = {
-  user : User
+  user: User
 }
 
-const UserAccountNav = ({user}: Props) => {
+const UserAccountNav = ({ user }: Props) => {
+
   return (
-    <div>UserAccountNav</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+<Button>hi</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className='font-medium'>{user.name}</p>}
+            {
+              user.email && <p className='w-[200] truncate text-sm text-zinc-700'>
+                {user.email}
+              </p>
+            }
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href='/'>
+            yaday
+          </Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+         
+         onClick={(e) => {
+          e.preventDefault()
+          signOut().catch(console.error)
+        }}        
+        className='text-red-600 cursor-pointer'
+        >
+          Sign Out
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
