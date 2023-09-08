b63373c0dff55fdef186bcafa9a7526c964875da
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-09-08 12:12:06 +0530

    endpoint for creating a game(quiz)

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -31,6 +31,59 @@ export async function POST(req: Request, res: Response) {
         topic,
       },
     });
+    const { data } = await axios.post(`${process.env.API_URL}/api/questions`, {
+      amount,
+      type,
+      topic,
+    });
+    if (type === 'mcq') {
+      type mcqQuestion = {
+        question: string;
+        answer: string;
+        option1: string;
+        option2: string;
+        option3: string;
+      };
+      let manyData = data.questions.map((question: mcqQuestion) => {
+        let options = [
+          question.answer,
+          question.option1,
+          question.option2,
+          question.option3,
+        ];
+        options = options.sort(() => Math.random() - 0.5);
+        return {
+          question: question.question,
+          answer: question.answer,
+          options: JSON.stringify(options),
+          gameId: game.id,
+          questionType: 'mcq',
+        };
+      });
+      await prisma.question.createMany({
+        data: manyData,
+      });
+    } else if (type === 'open_ended') {
+      type openQuestion = {
+        question: string;
+        answer: string;
+      };
+
+      let manyData = data.questions.map((question: openQuestion) => {
+        return {
+          question: question.question,
+          answer: question.answer,
+          gameId: game.id,
+          questionType: 'open_ended',
+        };
+      });
+      await prisma.question.createMany({
+        data: manyData,
+      });
+    }
+    return NextResponse.json({
+      gameId: game.id,
+    });
   } catch (error) {
     if (error instanceof ZodError) {
       return NextResponse.json(
