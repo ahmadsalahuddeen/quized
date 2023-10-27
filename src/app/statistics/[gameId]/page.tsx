914328c69a093c2045fed2dd414c0fcc7639c7fa
Swalah Codes <ahmadsalahuddeen6017@gmail.com>
2023-10-28 01:09:46 +0530

    result card ui

--- src/app/statistics/[gameId]/page.tsx
+++ src/app/statistics/[gameId]/page.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import ResultCard from '@/components/statistics/ResultCard';
+import { buttonVariants } from '@/components/ui/button';
+import { prisma } from '@/lib/db';
+import { getAuthSession } from '@/lib/nextauth';
+import { LucideLayoutDashboard } from 'lucide-react';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import React from 'react';
 
 type Props = {
   params: {
-    gameId: string
+    gameId: string;
+  };
+};
+
+const StatisticsPage = async ({ params: { gameId } }: Props) => {
+  const session = await getAuthSession();
+  if (!session) {
+    return redirect('/');
   }
-}
 
-const StatisticsPage = ({params: {gameId}}: Props) => {
+  const game = await prisma.game.findUnique({
+    where: { id: gameId },
+  });
+  if (!game) {
+    return redirect('quiz');
+  }
+
   return (
-    <div>{gameId}</div>
-  )
-}
+    <div className="p-8 mx-auto max-w-7xl ">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Statistics</h2>
+        <div className="flex items-center space-x-2">
+          <Link href="/dashboard" className={buttonVariants()}>
+            <LucideLayoutDashboard className="mr-2" />
+            Back to Dashboard
+          </Link>
+        </div>
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-6">
+        <ResultCard accuracy={80}/>
+        {/* <AccuracyCard/> */}
+        {/* <TimeTakenCard/> */}
+      </div>
 
-export default StatisticsPage
+      {/* <QuestionList/> */}
+    </div>
+  );
+};
+
+export default StatisticsPage;
